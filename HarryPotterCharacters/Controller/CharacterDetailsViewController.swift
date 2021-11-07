4e40678a1630e1807235be6409258d8b79bee73c
Matthew Hollyhead <matt.hollyhead@live.com>
2021-11-07 22:08:43 +0000

    Added character details view controller

---
+++ HarryPotterCharacters/Controller/CharacterDetailsViewController.swift
@@ -0,0 +1,58 @@
+//
+//  CharacterDetailsViewController.swift
+//  HarryPotterCharacters
+//
+//  Created by Matthew Hollyhead on 07/11/2021.
+//
+
+import UIKit
+
+class CharacterDetailsViewController: UIViewController {
+    
+    var name: String?
+    var gender: String?
+    var dateOfBirth: String?
+    var schoolHouse: String?
+    var patronus: String?
+    var actor: String?
+    
+    init?(coder: NSCoder, nameData: String, genderData: String, dateOfBirthData: String, houseData: String, patronusData: String, actorData: String) {
+        
+        self.name = nameData
+        self.gender = genderData
+        self.dateOfBirth = dateOfBirthData
+        self.schoolHouse = houseData
+        self.patronus = patronusData
+        self.actor = actorData
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(code) did not run")
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var dateOfBirthLabel: UILabel!
+    @IBOutlet weak var houseLabel: UILabel!
+    @IBOutlet weak var patronusLabel: UILabel!
+    @IBOutlet weak var actorLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        title = name
+        
+        nameLabel.text = "Character name: \(name ?? "No Data Found")"
+        genderLabel.text = "Gender: \(gender ?? "No Data Found")"
+        dateOfBirthLabel.text = "Date of birth: \(dateOfBirth ?? "No Data Found")"
+        houseLabel.text = "School House: \(schoolHouse ?? "No Data Found")"
+        patronusLabel.text = "Patronus: \(patronus ?? "No Data Found")"
+        actorLabel.text = "Actor: \(actor ?? "No Data Found")"
+        
+    }
+
+}
--- HarryPotterCharacters/Model/Character.swift
+++ HarryPotterCharacters/Model/Character.swift
@@ -9,10 +9,18 @@ import Foundation
 
 struct Character: Decodable {
     var name: String?
+    var gender: String?
     var house: String?
+    var dateOfBirth: String?
+    var patronus: String?
+    var actor: String?
     
     enum CodingKeys: String, CodingKey {
         case name = "name"
+        case gender = "gender"
         case house = "house"
+        case dateOfBirth = "dateOfBirth"
+        case patronus = "patronus"
+        case actor = "actor"
     }
 }
