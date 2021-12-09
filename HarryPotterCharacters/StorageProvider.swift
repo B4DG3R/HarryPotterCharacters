227c23e0ab8e21588e0d5c75fdb98090685a648c
B4DG3R <matt.hollyhead@live.com>
2021-12-09 06:30:23 +0000

    added coreData model and persistentContainer file

--- HarryPotterCharacters/StorageProvider.swift
+++ HarryPotterCharacters/StorageProvider.swift
@@ -15,4 +15,53 @@ class StorageProvider {
     
     static var shared = StorageProvider()
     
+    let persistentContainer: NSPersistentContainer
+    
+    init() {
+        persistentContainer = NSPersistentContainer(name: "HarryPotterCharacters")
+        
+        persistentContainer.loadPersistentStores(completionHandler: { description, error in
+            if let error = error {
+                fatalError("Core Data Store failed to load with error: \(error)")
+            }
+        })
+    }
+}
+
+extension StorageProvider {
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+                print("Context saved")
+            } catch {
+                let nerror = error as NSError
+                fatalError("unresolved error \(nerror), \(nerror.userInfo)")
+            }
+        }
+    }
+    
+    func getAllFavorites() -> [FavoriteCharacter] {
+        let fetchRequest: NSFetchRequest<FavoriteCharacter> = FavoriteCharacter.fetchRequest()
+        
+        do {
+            return try persistentContainer.viewContext.fetch(fetchRequest)
+        } catch {
+            print("Failed to fetch Characters \(error)")
+            return []
+        }
+    }
+    
+    func deleteFavoriteCharacter(_ character: FavoriteCharacter) {
+        persistentContainer.viewContext.delete(character)
+        
+        do {
+            try persistentContainer.viewContext.save()
+        } catch {
+            persistentContainer.viewContext.rollback()
+            print("failed to delete favorite character: \(error)")
+        }
+    }
 }
